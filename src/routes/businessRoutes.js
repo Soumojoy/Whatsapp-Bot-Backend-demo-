ea15e040fe8372e433930ffa35652659dee9186e
Soumojoy <soumojoybhattacharjee10@gmail.com>
2026-05-16 19:48:16 +0530

    Add payment options routes to businessRoutes
    Added payment options routes for creating, updating, and deleting payment options.

--- src/routes/businessRoutes.js
+++ src/routes/businessRoutes.js
@@ -11,5 +11,9 @@ router.put('/:id', authMiddleware, businessController.update);
 router.delete('/:id', authMiddleware, businessController.remove);
 router.get('/:id/onboard-accounts', authMiddleware, businessController.getOnboardAccounts);
 router.get('/:id/chats', authMiddleware, businessController.getChats);
+router.get('/:id/payment-options', authMiddleware, businessController.getPaymentOptions);
+router.post('/:id/payment-options', authMiddleware, businessController.createPaymentOption);
+router.put('/:id/payment-options/:optionId', authMiddleware, businessController.updatePaymentOption);
+router.delete('/:id/payment-options/:optionId', authMiddleware, businessController.deletePaymentOption);
 
 module.exports = router;
